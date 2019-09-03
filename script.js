2e3f88d9a057f89a76c02462b05e2e7245b035e9
DJ Knox <dhknox@gmail.com>
2019-09-03 19:04:02 -0400

    add paddle and ball objects

--- script.js
+++ script.js
@@ -2,23 +2,55 @@
 
 let height = 600;
 let width = 400;
-let paddleWidth = 50;
-let paddleHeight = 10;
+
+let paddle = {
+  width: 50,
+  height: 10,
+  x: 0
+};
+
+let ball = {
+  diameter: 15,
+  x: 0
+};
+
 
 function setup() {
   createCanvas(width, height);
 }
 
 function draw() {
+  paddle.x = constrain(mouseX - paddle.width / 2, 0, width - paddle.width);
+  ball.x = paddle.x + paddle.width / 2;
+  ball.y = height * 0.9 - ball.diameter / 2;
+
   clear();
+
+  drawBorders();
+  drawPaddle();
+  drawBall();
+}
+
+function drawBorders() {
+  noFill();
   rect(0, 0, width, height);
+}
 
-  let paddleX = constrain(mouseX - paddleWidth / 2, 0, width - paddleWidth);
-
+function drawPaddle() {
+  fill(50, 50, 255);
   rect(
-    paddleX,
-    height * 0.9, paddleWidth,
-    paddleHeight
+    paddle.x,
+    height * 0.9, paddle.width,
+    paddle.height
+  );
+}
+
+function drawBall() {
+  fill(255, 50, 50);
+  circle(
+    ball.x,
+    ball.y,
+    ball.diameter
   );
 }
 
